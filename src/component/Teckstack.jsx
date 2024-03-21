538258f127eccdf8bcc4d6d412d02a483790d3e9
Tohidul0 <tohidulalam967418@gmail.com>
2024-03-21 22:24:35 +0600

    teckstack updated

--- src/component/Teckstack.jsx
+++ src/component/Teckstack.jsx
@@ -6,7 +6,98 @@ function Teckstack(props) {
     return (
         <div className="teckStack Education" id="TeckStack">
             <h1 className="ED">TeckStack</h1>
-            <div className="row row-cols-2 row-cols-md-4 g-4">
+
+            <div className="row row-cols-2 row-cols-md-4 ">
+            <div className="row g-0 border border-white rounded-5  w-40 ">
+                <div className="col-md-6 mb-md-0 p-md-4">
+                    <img  className="img-fluid" style={{width: '90px', height: '90px'}} src="/src/assets/Images/skill/vscode-icons_file-type-html.svg"  alt="..."/>
+                </div>
+                <div className="col-md-6 p-4 ps-md-0 d-flex justify-content-center align-items-center justify-content-center ">
+                    <h5 className="mt-0">Columns with stretched link </h5>
+                </div>
+            </div>
+            <div className="row g-0 border border-white rounded-5 w-40">
+                <div className="col-md-6 mb-md-0 p-md-4">
+                    <img  className="img-fluid" style={{width: '90px', height: '90px'}} src="/src/assets/Images/skill/vscode-icons_file-type-css.svg"  alt="..."/>
+                </div>
+                <div className="col-md-6 p-4 ps-md-0 d-flex justify-content-center align-items-center justify-content-center ">
+                    <h5 className="mt-0">Columns with stretched link </h5>
+                </div>
+            </div>
+            <div className="row g-0 border border-white rounded-5  w-40">
+                <div className="col-md-6 mb-md-0 p-md-4">
+                    <img  className="img-fluid" style={{width: '90px', height: '90px'}} src="/src/assets/Images/skill/vscode-icons_file-type-js-official.svg"  alt="..."/>
+                </div>
+                <div className="col-md-6 p-4 ps-md-0 d-flex justify-content-center align-items-center justify-content-center ">
+                    <h5 className="mt-0">Columns with stretched link </h5>
+                </div>
+            </div>
+            <div className="row g-0 border border-white rounded-5  w-40">
+                <div className="col-md-6 mb-md-0 p-md-4">
+                    <img  className="img-fluid" style={{width: '90px', height: '90px'}} src="/src/assets/Images/skill/react-logo-vector-1.svg"  alt="..."/>
+                </div>
+                <div className="col-md-6 p-4 ps-md-0 d-flex justify-content-center align-items-center justify-content-center ">
+                    <h5 className="mt-0">Columns with stretched link </h5>
+                </div>
+            </div>
+            <div className="row g-0 border border-white rounded-5  w-40">
+                <div className="col-md-6 mb-md-0 p-md-4">
+                    <img  className="img-fluid" style={{width: '90px', height: '90px'}} src="/src/assets/Images/skill/reduxx.png"  alt="..."/>
+                </div>
+                <div className="col-md-6 p-4 ps-md-0 d-flex justify-content-center align-items-center justify-content-center ">
+                    <h5 className="mt-0">Columns with stretched link </h5>
+                </div>
+            </div>
+            <div className="row g-0 border border-white rounded-5  w-40">
+                <div className="col-md-6 mb-md-0 p-md-4">
+                    <img  className="img-fluid" style={{width: '90px', height: '90px'}} src="/src/assets/Images/skill/png-transparent-mongodb-original-wordmark-logo-icon-thumbnail.png"  alt="..."/>
+                </div>
+                <div className="col-md-6 p-4 ps-md-0 d-flex justify-content-center align-items-center justify-content-center ">
+                    <h5 className="mt-0">Columns with stretched link </h5>
+                </div>
+            </div>
+            <div className="row g-0 border border-white rounded-5  w-40">
+                <div class="col-md-6 mb-md-0 p-md-4">
+                    <img  className="img-fluid" style={{width: '90px', height: '90px'}} src="/src/assets/Images/skill/firebase img.png"  alt="..."/>
+                </div>
+                <div className="col-md-6 p-4 ps-md-0 d-flex justify-content-center align-items-center justify-content-center ">
+                    <h5 className="mt-0">Columns with stretched link </h5>
+                </div>
+            </div>
+            <div className="row g-0 border border-white rounded-5  w-40">
+                <div class="col-md-6 mb-md-0 p-md-4">
+                    <img  className="img-fluid" style={{width: '90px', height: '90px'}} src="/src/assets/Images/skill/vscode-icons_file-type-tailwind.svg"  alt="..."/>
+                </div>
+                <div className="col-md-6 p-4 ps-md-0 d-flex justify-content-center align-items-center justify-content-center ">
+                    <h5 className="mt-0">Columns with stretched link </h5>
+                </div>
+            </div>
+            <div class="row g-0 border border-white rounded-5  w-40">
+                <div class="col-md-6 mb-md-0 p-md-4">
+                    <img  className="img-fluid" style={{width: '90px', height: '90px'}} src="/src/assets/Images/skill/logos_bootstrap.svg"  alt="..."/>
+                </div>
+                <div class="col-md-6 p-4 ps-md-0 d-flex justify-content-center align-items-center justify-content-center ">
+                    <h5 class="mt-0">Columns with stretched link </h5>
+                </div>
+            </div>
+            <div class="row g-0 border border-white rounded-5  w-40">
+                <div class="col-md-6 mb-md-0 p-md-4">
+                    <img className="img-fluid" style={{width: '90px', height: '90px'}} src="/src/assets/Images/skill/c--logo-icon-17.png"  alt="..."/>
+                </div>
+                <div class="col-md-6 p-4 ps-md-0 d-flex justify-content-center align-items-center justify-content-center ">
+                    <h5 class="mt-0">Columns with stretched link </h5>
+                </div>
+            </div>
+            <div class="row g-0 border border-white rounded-5  w-40">
+                <div class="col-md-6 mb-md-0 p-md-4">
+                    <img className="img-fluid" style={{width: '90px', height: '90px'}} src="/src/assets/Images/skill/vscode-icons_file-type-vscode.svg"  alt="..."/>
+                </div>
+                <div class="col-md-6 p-4 ps-md-0 d-flex justify-content-center align-items-center justify-content-center ">
+                    <h5 class="mt-0">Columns with stretched link </h5>
+                </div>
+            </div>
+            </div>
+            {/* <div className="row row-cols-2 row-cols-md-4 g-4">
             
 
             <div class="timg col d-flex justify-content-center">
@@ -32,7 +123,7 @@ function Teckstack(props) {
             </div>
            
 
-        </div>
+        </div> */}
         
 
     </div>
